cd562d69ac0c9b0161572e6508b4757e333d6d66
Vishnuvarthanios1993 <vishnuvarthanios24@gmail.com>
2025-02-26 14:43:32 +0530

    Create FavoriteMovie+CoreDataClass.swift

---
+++ MovieApp/FavoriteMovie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteMovie+CoreDataClass.swift
+//  MovieApp
+//
+//  Created by Vishnuvarthan on 26/2/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteMovie)
+public class FavoriteMovie: NSManagedObject {
+
+}
