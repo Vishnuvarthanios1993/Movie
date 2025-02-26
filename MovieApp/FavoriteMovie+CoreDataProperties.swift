680244327e3e8a4f580c4fff75ec4eaafda9625a
Vishnuvarthanios1993 <vishnuvarthanios24@gmail.com>
2025-02-26 14:43:35 +0530

    Create FavoriteMovie+CoreDataProperties.swift

---
+++ MovieApp/FavoriteMovie+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteMovie+CoreDataProperties.swift
+//  MovieApp
+//
+//  Created by Vishnuvarthan on 26/2/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension FavoriteMovie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavoriteMovie> {
+        return NSFetchRequest<FavoriteMovie>(entityName: "FavoriteMovie")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var overview: String?
+    @NSManaged public var posterPath: String?
+    @NSManaged public var releaseDate: String?
+    @NSManaged public var title: String?
+
+}
+
+extension FavoriteMovie : Identifiable {
+
+}
